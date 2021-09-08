d1d5363aa6d5f34f82e9ffed283cb544f6d9795d
Sonika <sonikapoudyal5@gmail.com>
2021-09-08 12:01:55 -0400

    added picture in about me page

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
-// import coverImage from '../../assets/images/cover.jpg';
+import profile from '../../assets/images/profile-pic.jpg';
 import './style.css';
+
 
 function About() {
     return (
         <Card>
+            <Card.Img variant="left" src={profile} className="d-flex justify-content-center alter" alt="picture-of-me" />
             <Card.Body>
-            {/* <img src={coverImage} alt="cover"/> <br/> */}
-                <h2 className = "me-color"> About Me </h2>
-                <p className = "body-color">
-                Welcome to my E-Portfolio! <br />
-                My name is Sonika Poudyal and I graduated from Rutgers University with a degree of Bachelors in
-                Biology
-                Sciences.
-                Eventhough, I graduated with a biology major, I never felt accomplished.
-                After graduation, I worked in a lab for few years but I never enjoyed it.
-                I was always searching for something better and I realized that I wanted to go for a different
-                career
-                path.
-                After struggling for a few years to find my right career path, I finally found one as a Web
-                developer.
+                <h2 className="me-color"> About Me </h2>
+                <p className="body-color">
+                    Welcome to my E-Portfolio! <br />
+                    My name is Sonika Poudyal and I graduated from Rutgers University with a degree of Bachelors in
+                    Biology
+                    Sciences.
+                    Eventhough, I graduated with a biology major, I never felt accomplished.
+                    After graduation, I worked in a lab for few years but I never enjoyed it.
+                    I was always searching for something better and I realized that I wanted to go for a different
+                    career
+                    path.
+                    After struggling for a few years to find my right career path, I finally found one as a Web
+                    developer.
                 </p>
             </Card.Body>
         </Card>
