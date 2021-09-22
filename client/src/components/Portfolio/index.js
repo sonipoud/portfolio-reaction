277e23a916f7cee9130766e47b0358f3565d24bf
Sonika <sonikapoudyal5@gmail.com>
2021-09-22 18:02:03 -0400

    two more projects added

--- client/src/components/Portfolio/index.js
+++ client/src/components/Portfolio/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
-import {AiOutlineGithub} from 'react-icons/ai';
+import { AiOutlineGithub } from 'react-icons/ai';
 import budgetTracker from '../../assets/images/budget-tracker.jpg';
 import noSql from '../../assets/images/noSQL.jpg';
 import regex from '../../assets/images/regex.jpg';
@@ -17,6 +17,8 @@ import code from '../../assets/images/code-quiz.jpg';
 import password from '../../assets/images/password-generator.jpg';
 import run from '../../assets/images/run-buddy.jpg';
 import horizeon from '../../assets/images/horiseon.jpg';
+import searchForBooks from '../../assets/images/books.jpg';
+import component from '../../assets/images/compo.jpg';
 import './style.css'
 
 function Portfolio() {
@@ -25,11 +27,29 @@ function Portfolio() {
             <h2> My work </h2>
             <div className="row row-cols-1 row-cols-md-2 g-4 flex-row title-color">
                 <Card style={{ width: '40rem' }}>
+                    <Card.Img src={component} alt="budget" />
+                    <Card.Body>
+                        <Card.Title>Components</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">MERN</Card.Subtitle>
+                        <Card.Link href=""><AiOutlineGithub style={{ fontSize: '40px' }} /></Card.Link>
+                    </Card.Body>
+                </Card>
+
+                <Card style={{ width: '40rem' }}>
+                    <Card.Img src={searchForBooks} alt="budget" />
+                    <Card.Body>
+                        <Card.Title>Book Search</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">MERN</Card.Subtitle>
+                        <Card.Link href="https://secret-tor-92417.herokuapp.com/"><AiOutlineGithub style={{ fontSize: '40px' }} /></Card.Link>
+                    </Card.Body>
+                </Card>
+
+                <Card style={{ width: '40rem' }}>
                     <Card.Img src={budgetTracker} alt="budget" />
                     <Card.Body>
                         <Card.Title>PWA Budget-tracker</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted">JavaScript/Express/Mongoose/PWA</Card.Subtitle>
-                        <Card.Link href="https://budtrack-money.herokuapp.com/"><AiOutlineGithub style={{fontSize:'40px'}}/></Card.Link>
+                        <Card.Link href="https://budtrack-money.herokuapp.com/"><AiOutlineGithub style={{ fontSize: '40px' }} /></Card.Link>
                     </Card.Body>
                 </Card>
 
@@ -38,7 +58,7 @@ function Portfolio() {
                     <Card.Body>
                         <Card.Title>NoSQL</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted">JNoSQL/MongoDB/Express.js/Node</Card.Subtitle>
-                        <Card.Link href="https://github.com/sonipoud/NosocialSQL"><AiOutlineGithub style={{fontSize:'40px'}}/></Card.Link>
+                        <Card.Link href="https://github.com/sonipoud/NosocialSQL"><AiOutlineGithub style={{ fontSize: '40px' }} /></Card.Link>
                     </Card.Body>
                 </Card>
 
@@ -47,7 +67,7 @@ function Portfolio() {
                     <Card.Body>
                         <Card.Title>Regex</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted">Github Gist</Card.Subtitle>
-                        <Card.Link href="https://gist.github.com/sonipoud/9895a3c3ba5d2781a0d10d3c735bdc48"><AiOutlineGithub style={{fontSize:'40px'}}/></Card.Link>
+                        <Card.Link href="https://gist.github.com/sonipoud/9895a3c3ba5d2781a0d10d3c735bdc48"><AiOutlineGithub style={{ fontSize: '40px' }} /></Card.Link>
                     </Card.Body>
                 </Card>
 
@@ -56,7 +76,7 @@ function Portfolio() {
                     <Card.Body>
                         <Card.Title>Beat IT</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted">Sequelize/Node/JS/Express/Handlebars/CSS</Card.Subtitle>
-                        <Card.Link href="https://beatit-music-app.herokuapp.com/"><AiOutlineGithub style={{fontSize:'40px'}}/></Card.Link>
+                        <Card.Link href="https://beatit-music-app.herokuapp.com/"><AiOutlineGithub style={{ fontSize: '40px' }} /></Card.Link>
                     </Card.Body>
                 </Card>
 
@@ -65,7 +85,7 @@ function Portfolio() {
                     <Card.Body>
                         <Card.Title>Tech Blog</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted">Sequelize/Node/JS/Express/Handlebars/CSS</Card.Subtitle>
-                        <Card.Link href="https://techbloggermvc.herokuapp.com/"><AiOutlineGithub style={{fontSize:'40px'}}/></Card.Link>
+                        <Card.Link href="https://techbloggermvc.herokuapp.com/"><AiOutlineGithub style={{ fontSize: '40px' }} /></Card.Link>
                     </Card.Body>
                 </Card>
 
@@ -74,7 +94,7 @@ function Portfolio() {
                     <Card.Body>
                         <Card.Title>E-commerce Back End</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted">SQL/sequelize/Node/JS</Card.Subtitle>
-                        <Card.Link href="https://sonipoud.github.io/mercey/"><AiOutlineGithub style={{fontSize:'40px'}}/></Card.Link>
+                        <Card.Link href="https://sonipoud.github.io/mercey/"><AiOutlineGithub style={{ fontSize: '40px' }} /></Card.Link>
                     </Card.Body>
                 </Card>
 
@@ -83,7 +103,7 @@ function Portfolio() {
                     <Card.Body>
                         <Card.Title>Note Taker</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted">HTML/CSS/JS/Express/Node/API</Card.Subtitle>
-                        <Card.Link href="https://note-notetaker.herokuapp.com/"><AiOutlineGithub style={{fontSize:'40px'}}/></Card.Link>
+                        <Card.Link href="https://note-notetaker.herokuapp.com/"><AiOutlineGithub style={{ fontSize: '40px' }} /></Card.Link>
                     </Card.Body>
                 </Card>
 
@@ -92,7 +112,7 @@ function Portfolio() {
                     <Card.Body>
                         <Card.Title>Team Profile Generator</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted">JS/Node/OOP/TDD/ES6</Card.Subtitle>
-                        <Card.Link href="https://sonipoud.github.io/Antisfy/"><AiOutlineGithub style={{fontSize:'40px'}}/></Card.Link>
+                        <Card.Link href="https://sonipoud.github.io/Antisfy/"><AiOutlineGithub style={{ fontSize: '40px' }} /></Card.Link>
                     </Card.Body>
                 </Card>
 
@@ -101,7 +121,7 @@ function Portfolio() {
                     <Card.Body>
                         <Card.Title>ReadMe Generator</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted">JS</Card.Subtitle>
-                        <Card.Link href="https://sonipoud.github.io/genreadmeator/"><AiOutlineGithub style={{fontSize:'40px'}}/></Card.Link>
+                        <Card.Link href="https://sonipoud.github.io/genreadmeator/"><AiOutlineGithub style={{ fontSize: '40px' }} /></Card.Link>
                     </Card.Body>
                 </Card>
 
@@ -110,7 +130,7 @@ function Portfolio() {
                     <Card.Body>
                         <Card.Title>Brewery Dog</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted">JS</Card.Subtitle>
-                        <Card.Link href="https://jconnro.github.io/brewery-dog/"><AiOutlineGithub style={{fontSize:'40px'}}/></Card.Link>
+                        <Card.Link href="https://jconnro.github.io/brewery-dog/"><AiOutlineGithub style={{ fontSize: '40px' }} /></Card.Link>
                     </Card.Body>
                 </Card>
 
@@ -119,7 +139,7 @@ function Portfolio() {
                     <Card.Body>
                         <Card.Title>Weather Dashboard</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted">HTML/CSS/JS/API</Card.Subtitle>
-                        <Card.Link href="https://sonipoud.github.io/blazerer/"><AiOutlineGithub style={{fontSize:'40px'}}/></Card.Link>
+                        <Card.Link href="https://sonipoud.github.io/blazerer/"><AiOutlineGithub style={{ fontSize: '40px' }} /></Card.Link>
                     </Card.Body>
                 </Card>
 
@@ -128,7 +148,7 @@ function Portfolio() {
                     <Card.Body>
                         <Card.Title>WorkDay Scheduler</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted">HTML/CSS/JS/</Card.Subtitle>
-                        <Card.Link href="https://sonipoud.github.io/leafcal/"><AiOutlineGithub style={{fontSize:'40px'}}/></Card.Link>
+                        <Card.Link href="https://sonipoud.github.io/leafcal/"><AiOutlineGithub style={{ fontSize: '40px' }} /></Card.Link>
                     </Card.Body>
                 </Card>
 
@@ -137,7 +157,7 @@ function Portfolio() {
                     <Card.Body>
                         <Card.Title>Code Quiz</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted">HTML/CSS/JS/</Card.Subtitle>
-                        <Card.Link href="https://sonipoud.github.io/QAMPLY/"><AiOutlineGithub style={{fontSize:'40px'}}/></Card.Link>
+                        <Card.Link href="https://sonipoud.github.io/QAMPLY/"><AiOutlineGithub style={{ fontSize: '40px' }} /></Card.Link>
                     </Card.Body>
                 </Card>
 
@@ -146,7 +166,7 @@ function Portfolio() {
                     <Card.Body>
                         <Card.Title>Password Generator</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted">HTML/CSS/JS/</Card.Subtitle>
-                        <Card.Link href="https://sonipoud.github.io/crashgen/"><AiOutlineGithub style={{fontSize:'40px'}}/></Card.Link>
+                        <Card.Link href="https://sonipoud.github.io/crashgen/"><AiOutlineGithub style={{ fontSize: '40px' }} /></Card.Link>
                     </Card.Body>
                 </Card>
 
@@ -155,7 +175,7 @@ function Portfolio() {
                     <Card.Body>
                         <Card.Title>Run Buddy</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted">HTML/CSS</Card.Subtitle>
-                        <Card.Link href="https://sonipoud.github.io/run-buddy/"><AiOutlineGithub style={{fontSize:'40px'}}/></Card.Link>
+                        <Card.Link href="https://sonipoud.github.io/run-buddy/"><AiOutlineGithub style={{ fontSize: '40px' }} /></Card.Link>
                     </Card.Body>
                 </Card>
 
@@ -164,7 +184,7 @@ function Portfolio() {
                     <Card.Body>
                         <Card.Title>Horiseon</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted">HTML/CSS</Card.Subtitle>
-                        <Card.Link href="https://sonipoud.github.io/mahamandir/"><AiOutlineGithub style={{fontSize:'40px'}}/></Card.Link>
+                        <Card.Link href="https://sonipoud.github.io/mahamandir/"><AiOutlineGithub style={{ fontSize: '40px' }} /></Card.Link>
                     </Card.Body>
                 </Card>
             </div>
